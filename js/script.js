8770d00eff395651f82ce164053269c88da726ec
Ali1266 <105448089+Ali1266@users.noreply.github.com>
2022-12-23 14:18:03 +0400

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,22 @@
+const btnSkills = document.querySelector('#btnSkills');
+const btnPortfolio = document.querySelector('#btnPortfolio');
+
+btnSkills.onclick = function() {
+    document.getElementById('portfolio').style.display = 'none';
+    document.getElementById('skills').style.display = 'block';
+    document.getElementById('btnSkills').classList.add('active');
+    document.getElementById('btnPortfolio').classList.remove('active');
+}
+
+btnPortfolio.onclick = function() {
+    document.getElementById('portfolio').style.display = 'grid';
+    document.getElementById('skills').style.display = 'none';
+    document.getElementById('btnSkills').classList.remove('active');
+    document.getElementById('btnPortfolio').classList.add('active');
+}
+
+document.body.onclick = function() {
+    document.body.style.background = '#262a41';
+    document.getElementById('moon').style.display = 'none';
+    document.getElementById('sun').style.display = 'block';
+}
