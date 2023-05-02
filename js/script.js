066f3ada0ebb6b6fde9c600721b90db5c0507108
Ali <apollon.play15@gmail.com>
2023-05-03 00:24:08 +0400

    -_-

---
+++ js/script.js
@@ -0,0 +1,42 @@
+$(function slider(){
+    $('.slick-slider').slick({
+        dots: true,
+        fade: true,
+        autoplay: true,
+        autoplayspeed: 2000
+    });
+});
+
+document.addEventListener('DOMContentLoaded', ()=>{
+    const menu = document.querySelector('.menu'),
+          menuBtns = document.querySelectorAll('.menu__btn'),
+          blocks = document.querySelectorAll('.block');
+
+    
+    function displayNone(){
+        blocks.forEach(item=>{
+            item.style.display = 'none';
+        });
+        menuBtns.forEach(item=>{
+            item.classList.remove('active');
+        });
+    }
+
+    function displayBlock(i=0){
+        blocks[i].style.display = 'block';
+        menuBtns[i].classList.add('active');
+    }
+
+    displayNone();
+    displayBlock();
+
+    menu.addEventListener('click', (event)=>{
+        const target = event.target;
+        menuBtns.forEach((item, i)=>{
+            if(target == item){
+                displayNone();
+                displayBlock(i);
+            }
+        });
+    });
+});
